7749da1ac2fcc28b6a176941adb5287d3510902e
Tony Greeley <tony.greeley@me.com>
2018-12-08 10:18:43 -0600

    seeds.sql file complete, both sql files are complete

--- db/schema.sql
+++ db/schema.sql
@@ -6,5 +6,5 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     burger_name VARCHAR(255) NOT NULL,
-    devoured BOOLEAN NOT NULL
+    devoured BOOLEAN DEFAULT false
 )
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name)
+VALUES (Sleep Burger),
+(Au Cheval),
+(Quarter Pounder);
