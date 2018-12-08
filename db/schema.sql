abaec2747bd56ecde3f7f149d1243de2b6871f96
Tony Greeley <tony.greeley@me.com>
2018-12-08 10:09:01 -0600

    schema.sql file complete

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE burgers_db IF EXISTS;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN NOT NULL
+)
