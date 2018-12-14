2419d408c1b71566d17951dcfbb758ee7d70b46c
Tony Greeley <tony.greeley@me.com>
2018-12-14 03:08:59 -0600

    made changes to put route

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -23,7 +23,7 @@ router.post("/api/burgers", (req, res) => {
         });
 });
 
-router.put("/api/burgers/:id", function (req, res) {
+router.put("/:id", function (req, res) {
     const condition = "id = " + req.params.id;
     console.log("Condition", condition);
     burger.updateOne({
