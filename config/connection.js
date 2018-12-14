f42d0a5979acd23fc7070484eeb45aab1f75d604
Tony Greeley <tony.greeley@me.com>
2018-12-13 20:55:58 -0600

    updated connection file to work with JAWSDB for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -3,22 +3,28 @@ const mysql = require("mysql");
 require('dotenv').config();
 
 const sqlPw = process.env.sql_password
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: sqlPw,
-    database: "burgers_db"
-});
+let connection;
 
-// Make connection.
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: sqlPw,
+        database: "burgers_db"
 
-// Export connection for our ORM to use.
-module.exports = connection;
+    });
+
+    // Make connection.
+    connection.connect(function (err) {
+        if (err) {
+            console.error("error connecting: " + err.stack);
+            return;
+        }
+        console.log("connected as id " + connection.threadId);
+    });
+
+    // Export connection for our ORM to use.
+    module.exports = connection;
