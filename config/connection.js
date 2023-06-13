278c0265c1c908accd64c61878b4e35672ea576c
Tony Greeley <tony.greeley@me.com>
2023-06-12 19:12:24 -0500

    Prepare app for deploy to Render

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,28 @@
 // Set up MySQL connection.
-const mysql = require("mysql");
+const mysql = require('mysql');
 require('dotenv').config();
 
-const sqlPw = process.env.sql_password;
+const sqlPw = process.env.SQL_PASSWORD;
 let connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.MYSQL_URL) {
+  connection = mysql.createConnection(process.env.MYSQL_URL);
 } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: sqlPw,
-        database: "burgers_db"
-
-    });
-};
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: sqlPw,
+    database: 'burgers_db'
+  });
+}
 // Make connection.
 connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
 });
 
 // Export connection for our ORM to use.
